0e986cfae7ac8e8b5d4f0d70707931a6e6116011
James Muturi <james.muturi@andela.com>
2016-07-08 19:01:51 +0300

    Setup the client project

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 
+const publicPath = path.resolve(__dirname, 'client/dist/');
+
 const app = express();
+app.use(express.static(publicPath));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
-app.get('/', (req, res) => res.send({
-  message: 'Welcome',
-}));
+app.get('/', (req, res) => res.sendFile(
+  path.resolve(__dirname, 'client/dist/index.html')
+));
 
 module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: path.resolve(__dirname, 'client/public/index.html'),
+  filename: 'index.html',
+  inject: 'body',
+});
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'client/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+    }],
+  },
+  resolve: ['', '.js', '.jsx'],
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
+};
